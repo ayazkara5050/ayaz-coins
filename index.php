9654ce751ca683400fbdefb1870ce1f65641921a
ayazkara5050 <49233792+ayazkara5050@users.noreply.github.com>
2019-04-03 13:43:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <meta http-equiv="refresh" content="10;URL=index.php.php">
 <title>Coins</title>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js">
 </script>
