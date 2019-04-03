d949256ca4dc637cce18173c99edb3d50a097909
ayazkara5050 <49233792+ayazkara5050@users.noreply.github.com>
2019-04-03 13:32:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coins</title>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js">
+</script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+</head>
+
+<body>
+<div id="veri">
+
+</div>
+
+</body>
+</html>
+
+<script type="text/javascript">
+   $(document).ready(function() {
+       $("#veri").load("veri.php");
+     var refreshId = setInterval(function() {
+        $("#veri").load('veri.php');
+     }, 10000);
+  });
+</script>
